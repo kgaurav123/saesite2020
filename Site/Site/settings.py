bc8076e7cc3936a5edb2edfe63a0f1feb7cbb81a
kgaurav123 <kumar123gaueav.kg@gmail,com>
2020-03-31 17:29:01 +0530

    second commit

--- Site/Site/settings.py
+++ Site/Site/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
