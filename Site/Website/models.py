4548c948fa7e180bd14581f0a2a4b14dbfe4d2cd
ricky1812 <rickydas189@gmail.com>
2020-04-02 22:19:55 +0530

    added views

--- Site/Website/models.py
+++ Site/Website/models.py
@@ -1,7 +1,59 @@
 from django.db import models
 from datetime import date,time
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save 
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+
+def validate_image_size(value):
+    limit = 1 * 1024 * 1024
+    if value.size > limit:
+        raise ValidationError('File too large. Size should not exceed 1 MiB.')
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    yr_choices = [
+        (1,'First Year'),
+        (2,'Second Year'),
+        (3,'Third Year'),
+        (4,'Final Year'),
+    ]
+    dept_choices = [
+        ('OFFICE BEARERS','Office Bearers'),
+        ('MANAGEMENT','Management'),
+        ('AUTOMOBILE','Automobile'),
+        ('MANUAL','Manual'),
+        ('WEBD','Web Developement'),
+        ('GRAPHICS','Graphics and Photography'),
+        ('NDORS','NDORS'),
+        ]
+
+    
+    first_name=models.CharField(max_length=50,null=False)
+    last_name = models.CharField(max_length = 50)
+    year = models.IntegerField(choices = yr_choices,default=2)
+    department = models.CharField(max_length=20,choices = dept_choices,default='FIRST')
+    fb=models.URLField(null=True, blank=True)
+    email=models.EmailField(null=True, blank=True)
+    phone=models.CharField(null=True,blank=False, max_length=15)
+    photo = models.ImageField(null=True, blank=False,upload_to='profile_pics',validators=[validate_image_size])
+
+    def __str__(self):
+        #return (self.first_name + self.last_name)
+        return (str(self.user))
+
+@receiver(post_save,sender=User)
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
 
 class Member(models.Model):
     yr_choices = [
--- Site/Website/urls.py
+++ Site/Website/urls.py
@@ -3,5 +3,9 @@ from .import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('baja/',views.baja,name='baja'),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.login_view,name='login'),
+    path('logout/',views.logout_view,name='logout'),
+    path('update/',views.update_profile,name='update_profile'),
     
 ]
