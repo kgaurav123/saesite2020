ec6c808fd00e051198b2dc90a3b74c0e490c45cb
ricky1812 <rickydas189@gmail.com>
2020-04-03 14:24:13 +0530

    conflicts1

--- Site/Website/urls.py
+++ Site/Website/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/',views.login_view,name='login'),
     path('logout/',views.logout_view,name='logout'),
     path('update/',views.update_profile,name='update_profile'),
-    path('member_list/',views.member_list,name='member_list'),
+    path('members/',views.member_list,name='member_list'),
     
     
 ]
